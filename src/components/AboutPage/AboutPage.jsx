bee2d189314ccbbf82fdb64c407680dd59988d16
Austin Flatt <austindflatt@gmail.com>
2022-02-04 14:32:03 -0500

    updated about me section and added crypto currency project

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -20,8 +20,8 @@ function AboutPage() {
         <br />
         <ContainerSmall>
         <h2>About Me</h2>
-        <p>I'm a Software Engineer with a passion for everything front-end. I love building and creating, whether those things are web applications, mobile apps, or designing in Photoshop. Ever since I can remember, I've always had an interest and passion for tech. I specialize in front-end development and make it my mission to create accessible and user-friendly applications and websites that run extremely fast.</p>
-        <p>Other than coding, I spend most of my free time traveling or investing in Crypto. I've also been reading more lately so feel free to reach out to me with any book recommendations!</p>
+        <p>I'm a Software Engineer with a passion for everything front-end. I love building and creating, whether those things are web applications, or designing in Photoshop. I'm a problem solver and I just love watching my thoughts come to life on screen. Ever since I can remember, I've always had an interest and passion for tech. I specialize in front-end development and make it my mission to create accessible and user-friendly applications and websites.</p>
+        <p>Other than coding, I spend most of my free time playing video games or reading! With the latter being reading, so feel free to reach out to me with any book recommendations!</p>
         <h2>Favorite Books</h2>
         <p>
             <ul >
