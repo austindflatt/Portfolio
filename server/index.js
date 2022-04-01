ba9e41e93f735ec572fc81b207515275ff5eedee
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-04-01 00:59:42 -0400

    user update route is working

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoute = require('./routes/auth')
+const authRoute = require('./routes/auth');
+const usersRoute = require('./routes/users');
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose
 
 app.use(express.json());
 app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
 
 app.listen(3001, () => {
 	console.log('Server is running...')
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,5 +2,41 @@ const router = require('express').Router();
 const User = require('../models/usersModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { verify } = require('./verifyToken');
 
 // UPDATE
+router.put('/update/:id', async (req, res) => {
+	const slicedToken = req.headers.authorization.slice(7);
+	const decodedToken = jwt.verify(slicedToken, process.env.SECRET_KEY);
+
+	res.locals.decodedToken = decodedToken;
+	const foundUser = await User.findOne({ email: decodedToken.email });
+
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	req.body.password = hashedPassword;
+
+	if(foundUser._id.toString() === req.params.id) {
+		try {
+			const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+				new: true,
+			});
+			res.status(200).json({ message: 'Updated user', payload: updatedUser });
+		} catch (error) {
+			res.status(500).json({ message: error, error: error.message });
+		} 
+	} else {
+		return res.status(403).json({
+			message: 'You can only update your account'
+		})
+	}
+})
+
+// DELETE
+
+// GET USERS
+
+// GET ALL USERS
+
+module.exports = router;
